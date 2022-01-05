2aa03d7ff4b6623221fe7911d42caab01b72ef63
Mihow <mihow.bogucki@gmail.com>
2022-01-05 16:22:21 +0100

    Seed car

--- repository/repository/Program.cs
+++ repository/repository/Program.cs
@@ -1,18 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Npgsql;
+using repository;
 
 Console.WriteLine("Hello, Postgres!");
 
-var cs = "Host=localhost:5433;Username=root;Password=password;Database=postgres";
+const string cs = "Host=localhost:5433;Username=root;Password=password;Database=postgres";
 
 using var con = new NpgsqlConnection(cs);
 con.Open();
 
-var sql = "SELECT version()";
-
-using var cmd = new NpgsqlCommand(sql, con);
-
-var version = cmd.ExecuteScalar().ToString();
-Console.WriteLine($"PostgreSQL version: {version}");
-
+Car.Seed(con);
